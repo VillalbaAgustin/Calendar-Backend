a08187299b76aee47aaecb86f231bc86eb460f3d
VillalbaAgustin <villalba.agus.99@gmail.com>
2022-12-05 17:29:56 -0300

    Calendar-Backend#8

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ app.use('/api/auth', require('./routes/auth'));
 // TODO: CRUD: Eventos
 app.use('/api/events', require('./routes/events'));
 
-
+app.get('*', (req, res) => {
+  res.sendFile( __dirname + '/public/index.html');
+})
 
 // Escuchar peticiones
 
